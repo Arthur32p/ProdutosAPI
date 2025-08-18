3eb00a66b3705aaf9c6b2890e9c5342f1e31e8b6
Arthur Lelis Uchoa <arthurlelisuchoa32@gmail.com>
2025-08-18 11:29:42 -0300

    Deletarprodutos

---
+++ ProdutoController.java
@@ -0,0 +1,42 @@
+package io.github.arthur32.produtosapi.controller;
+
+import io.github.arthur32.produtosapi.model.Produto;
+import io.github.arthur32.produtosapi.repository.ProdutoRepository;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
+import java.util.UUID;
+
+@RestController
+@RequestMapping("produtos")
+public class ProdutoController {
+    private ProdutoRepository produtoRepository;
+
+    public ProdutoController(ProdutoRepository produtoRepository) {
+        this.produtoRepository = produtoRepository;
+    }
+
+    @PostMapping
+    public Produto salvar(@RequestBody Produto produto){
+        System.out.println("Produto recebido: " + produto);
+
+        var id = UUID.randomUUID().toString();
+        produto.setId(id);
+
+        produtoRepository.save(produto);
+        return produto;
+    }
+
+    @GetMapping("{id}")
+    public Produto obterProduto(@PathVariable("id") String id){
+        //Optional<Produto> produto = produtoRepository.findById(id);
+        //return produto.isPresent() ? produto.get() : null;
+
+        return produtoRepository.findById(id).orElse(null);
+    }
+
+    @DeleteMapping("{id}")
+    public void deletar(@PathVariable("id") String id){
+        produtoRepository.deleteById(id);
+    }
+}
