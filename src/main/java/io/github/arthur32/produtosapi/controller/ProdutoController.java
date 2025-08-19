0924fa22f07192cd426c594769d409db3fca74cb
Arthur Lelis Uchoa <arthurlelisuchoa32@gmail.com>
2025-08-19 09:52:36 -0300

    BuscarProdutosPorNome

--- src/main/java/io/github/arthur32/produtosapi/controller/ProdutoController.java
+++ src/main/java/io/github/arthur32/produtosapi/controller/ProdutoController.java
@@ -4,6 +4,7 @@ import io.github.arthur32.produtosapi.model.Produto;
 import io.github.arthur32.produtosapi.repository.ProdutoRepository;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
@@ -44,5 +45,10 @@ public class ProdutoController {
     public void atualizar(@PathVariable("id") String id, @RequestBody Produto produto){
         produto.setId(id);
         produtoRepository.save(produto);
+    }
+
+    @GetMapping
+    public List<Produto> buscar(@RequestParam("nome") String nome){
+        return produtoRepository.findByNome(nome);
     }
 }
