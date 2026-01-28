d55ca28099abc462195b85261ae7a28976ff024c
Arthur32p <arthurlelisuchoa32@gmail.com>
2026-01-28 15:04:19 -0300

    Criando banco e persistindo produtos no banco

---
+++ src/main/java/io/github/arthur32p/produtosapi/repository/ProdutoRepository.java
@@ -0,0 +1,7 @@
+package io.github.arthur32p.produtosapi.repository;
+
+import io.github.arthur32p.produtosapi.model.Produto;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProdutoRepository extends JpaRepository<Produto, String> {
+}
